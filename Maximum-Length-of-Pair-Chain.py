af6c3bca7cb0022adf0270ab68308aea921f2e9f
San Ayush <41052723+San1357@users.noreply.github.com>
2021-11-23 20:00:24 +0530

    Create Maximum-Length-of-Pair-Chain.py

---
+++ Maximum-Length-of-Pair-Chain.py
@@ -0,0 +1,20 @@
+# UsinG LIS
+# Time complexity  : O(N^2)
+# Space complexity : O(N)
+
+
+
+#CODE :
+
+class Solution(object): #Time Limit Exceeded
+    def findLongestChain(self, pairs):
+       pair = sorted(pairs)
+       
+       dp = [1]*len(pair)
+        
+       for i in range(1, len(pair)):
+            for j in range(i):
+                if pair[i][1] < pair[j][0]:
+                    dp[i] = max(dp[i]+dp[j]+1)
+       return max(dp)
+          
